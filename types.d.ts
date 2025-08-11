0cefe9880fa5628a78c994c0fcd54d507ccfe253
Burooj <burooj.bj@gmail.com>
2025-08-11 15:58:20 -0400

    feat: render strudel code blocks

---
+++ types.d.ts
@@ -0,0 +1,8 @@
+declare module "@strudel/codemirror";
+declare module "@strudel/core";
+declare module "@strudel/transpiler";
+declare module "@strudel/webaudio";
+declare module "@strudel/soundfonts";
+declare module "@strudel/mini";
+declare module "@strudel/tonal";
+
